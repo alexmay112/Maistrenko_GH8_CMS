0338eaeaf076508e2e5062377c6830b87f11ae86
Maistrenko Oleksandr <oleksandr.mais@gmail.com>
2019-01-27 09:51:40 +0200

    HW15 useless code

--- Home15.loc/index.php
+++ Home15.loc/index.php
@@ -7,6 +7,53 @@
 </head>
 <body>
 
+<?php
+$unSortList = [3, 15, 59, 2, 23, 45, 7, 18, 2, 10, 33, 17];
+function sortArr($unSortList)
+{
+    $sort = [];
+    $sort[0] = $unSortList[0];
+    foreach ($unSortList as $key => $item) {
+//        array_unshift($sort, $unSortList[$key]);
+        for ($i = 0; $i <= count($sort); $i++){
+            if (count($sort) > 1 && $sort[$key] > $sort[$key + 1]) {
+                $tempVal = $sort[$key];
+                $sort[$key] = $sort[$key + 1];
+                $sort[$key + 1] = $tempVal;
+            }
+        }
+    }
+    return $sort;
+}
+
+$res = sortArr($unSortList);
+print_r($res);
+
+//function sortArr($unSortList)
+//{
+//    $sort = [];
+//    $i = 0;
+//    for ($i = 0; $i < count($unSortList); $i++) {
+//    array_unshift($sort, $unSortList[$i]);
+//    if (count($sort) > 1) {
+//
+//    }
+//        for ($j = 0; $j < count($sort); $j++) {
+//            if ($unSortList[$i] > $sort[$j]) {
+//                $oneHalf = array_splice($sort, $j);
+//                $oneHalf[] = $unSortList[$i];
+//                $twoHalf = array_slice($sort, $j);
+//                $sort = array_merge($oneHalf, $twoHalf);
+//            }
+//        }
+//    }
+//    print_r($unSortList);
+//    print_r($sort);
+//}
+//
+
+?>
+
 <script src="assets/js/libs.js"></script>
 <script src="assets/js/main.js"></script>
 </body>
