8ab6665f1434d6174c77ed27b3022eed81ee7798
Maistrenko Oleksandr <oleksandr.mais@gmail.com>
2019-01-27 14:05:47 +0200

    HW15 fix code

--- Home15.loc/index.php
+++ Home15.loc/index.php
@@ -8,50 +8,44 @@
 <body>
 
 <?php
-$unSortList = [3, 15, 59, 2, 23, 45, 7, 18, 2, 10, 33, 17];
-function sortArr($unSortList)
-{
-    $sort = [];
-    $sort[0] = $unSortList[0];
-    foreach ($unSortList as $key => $item) {
-//        array_unshift($sort, $unSortList[$key]);
-        for ($i = 0; $i <= count($sort); $i++){
-            if (count($sort) > 1 && $sort[$key] > $sort[$key + 1]) {
-                $tempVal = $sort[$key];
-                $sort[$key] = $sort[$key + 1];
-                $sort[$key + 1] = $tempVal;
-            }
-        }
-    }
-    return $sort;
-}
-
-$res = sortArr($unSortList);
-print_r($res);
 
 //function sortArr($unSortList)
 //{
 //    $sort = [];
-//    $i = 0;
-//    for ($i = 0; $i < count($unSortList); $i++) {
-//    array_unshift($sort, $unSortList[$i]);
-//    if (count($sort) > 1) {
-//
-//    }
-//        for ($j = 0; $j < count($sort); $j++) {
-//            if ($unSortList[$i] > $sort[$j]) {
-//                $oneHalf = array_splice($sort, $j);
-//                $oneHalf[] = $unSortList[$i];
-//                $twoHalf = array_slice($sort, $j);
-//                $sort = array_merge($oneHalf, $twoHalf);
+//    $sort[0] = $unSortList[0];
+//    foreach ($unSortList as $key => $item) {
+////        array_unshift($sort, $unSortList[$key]);
+//        for ($i = 0; $i <= count($sort); $i++){
+//            if (count($sort) > 1 && $sort[$key] > $sort[$key + 1]) {
+//                $tempVal = $sort[$key];
+//                $sort[$key] = $sort[$key + 1];
+//                $sort[$key + 1] = $tempVal;
 //            }
 //        }
 //    }
-//    print_r($unSortList);
-//    print_r($sort);
+//    return $sort;
 //}
 //
-
+//$res = sortArr($unSortList);
+//print_r($res);
+$unSortList = [3, 15, 59, 2, 23, 45, 7, 18, 2, 10, 33, 17];
+function sortArr($unSortList)
+{
+    $sort = [];
+    for ($i = 0; $i < count($unSortList); $i++) {
+        for ($j = 0; $j < count($sort); $j++) {
+            if (!isset($sort[$j + 1]) && $unSortList[$i] > $sort[$j]) {
+                array_push($sort, $unSortList[$i]);
+            }
+            if ($unSortList[$i] > $sort[$j] && $unSortList[$i] < $sort[$j + 1]) {
+                array_splice($sort, $j, 0, $unSortList[$i]);
+            }
+        }
+    }
+    print_r($unSortList);
+    print_r($sort);
+}
+sortArr($unSortList);
 ?>
 
 <script src="assets/js/libs.js"></script>
