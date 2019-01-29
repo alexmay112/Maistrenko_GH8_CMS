72d46fbe54859a247ce5fdc12a2cce02566ea5c5
Maistrenko Oleksandr <oleksandr.mais@gmail.com>
2019-01-29 17:15:28 +0200

    HW16 add html structure

--- Home16.loc/index.php
+++ Home16.loc/index.php
@@ -2,62 +2,57 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Insertion sort</title>
+    <title>Registration form</title>
     <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
 
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8 col-md-4 m-auto form-block">
+            <form>
+                <div class="form-group">
+                    <label for="inputUsername">Username</label>
+                    <input type="text" class="form-control" id="inputUsername" placeholder="Enter Username">
+                </div>
+                <div class="form-group">
+                    <label for="inputEmail">Email address</label>
+                    <input type="email" class="form-control" id="inputEmail" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword">Password</label>
+                    <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                </div>
+                <div class="form-group">
+                    <label for="inputConfirmPassword">Confirm Password</label>
+                    <input type="password" class="form-control" id="inputConfirmPassword" placeholder="Confirm Password">
+                </div>
+                <div class="form-group">
+                    <label for="inputFirstName">First name</label>
+                    <input type="text" class="form-control" id="inputFirstName" placeholder="Enter First name">
+                </div>
+                <div class="form-group">
+                    <label for="inputLastName">Last name</label>
+                    <input type="text" class="form-control" id="inputLastName" placeholder="Enter Last name">
+                </div>
+                <div class="form-group">
+                    <label for="inputAge">Age</label>
+                    <input type="number" min="7" class="form-control" id="inputAge" placeholder="Enter age">
+                </div>
+                <div class="form-group">
+                    <label for="formControlSelect">Gender</label>
+                    <select class="form-control" id="formControlSelect">
+                        <option>male</option>
+                        <option>female</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+
 <?php
-
-$arr = [3, 15, 59, 2, 23];
-
-function sortArr ($arr) {
-    for($i=0; $i<count($arr); $i++){
-        for($j=$i+1; $j<count($arr); $j++){
-            if($arr[$i]>$arr[$j]){
-                $temp = $arr[$j];
-                $arr[$j] = $arr[$i];
-                $arr[$i] = $temp;
-            }
-        }
-    }
-    print_r($arr);
-}
-
-sortArr($arr)
-
-
-
-
-
-
-
-
-
-
-//function sortArr($unSortList)
-//{
-//    $sort[0] = $unSortList[0];
-//    for ($i = 1; $i < count($unSortList); $i++) {
-//        for ($j = 1; $unSortList[$i] > $sort[$j] && $unSortList[$i] < $sort[$j + 1]; $j++) {
-//
-//
-//            if (!isset($sort[$j + 1]) && $unSortList[$i] > $sort[$j]) {
-//                $sort[] = $unSortList[$i];
-//            } elseif (!isset($sort[$j + 1]) && $unSortList[$i] < $sort[$j]) {
-//                array_unshift($sort, $unSortList[$i]);
-//            } elseif ($unSortList[$i] > $sort[$j] && $unSortList[$i] < $sort[$j + 1]) {
-//                array_splice($sort, $j, 0, $unSortList[$i]);
-//            } else {
-//                array_splice($sort, $j - 1, 0, $unSortList[$i]);
-//            }
-//        }
-//    }
-//    print_r($unSortList);
-//    print_r($sort);
-//}
-//
-//sortArr($unSortList);
 
 ?>
 
